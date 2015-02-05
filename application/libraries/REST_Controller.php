751b00517eb8cb8f5ef74aa894373c36d47dd5bd
zhy654 <zhuyibing324@qq.com>
2015-02-05 18:03:59 +0800

    The end of the output lost behind
    line:503
    header('Content-Length: ' . strlen($output));
    
    
    I added 20 characters in length, behind the symbols behind the output is not lost, you have not met?

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -500,7 +500,7 @@ abstract class REST_Controller extends CI_Controller
         // the reduction, causing the browser to hang waiting for more data.
         // We'll just skip content-length in those cases.
         if ( ! $this->_zlib_oc && ! $this->config->item('compress_output')) {
-            header('Content-Length: ' . strlen($output));
+            header('Content-Length: ' . strlen($output)+20);
         }
 
         if($continue){
